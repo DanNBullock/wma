59c1c86bb702a5f8fe8b5fb5f44012163d0cbc56
DanNBullock <dnbulloc@umail.iu.edu>
2017-06-26 18:17:06 -0400

    Update wma_wrapper.m
    edited for classification structure compatibility

--- wma_wrapper.m
+++ wma_wrapper.m
@@ -46,7 +46,14 @@ function [tracts_indexes] = wma_wrapper(wbFG,dt6,FiberDir,saveHeader,fsDIR, nosa
 % loads file if a string was passed 
 if ischar(wbFG)
     wbFG = load(wbFG);
+    %if it is a fe structure, get the wbFG out of it
+    if isfield(wbFG, 'fe')
+        wbFG = feGet(wbFG.fe, 'fibers acpc');
+    end
 else
+    if isfield(wbFG, 'fg')
+        wbFG = feGet(wbFG, 'fibers acpc');
+    end
 end
 
 %if it is a fe structure, get the wbFG out of it
@@ -67,67 +74,45 @@ if notDefined('nosave'), nosave=false;end
 
 disp('Segmenting Major and Associative Tracts');
 
+% Segment the major white matter tracts in the Mori Atlas
+tic
+[fg_classified,~,classification,~] = wma_majortracts(dti_file, wbFG);
+toc=segmentTime;
+fprintf ('\n Mori Atlas segmentation run complete in %4.2f hours \n', segmentTime/(60*60))
+
+% update name field
+newTractNames={'L_VOF','R_VOF','L_pArc','R_pArc','L_TPC','R_TPC','L_MdLF','R_MdLF'};
+for iNEWtracts=21:28
+   classification.names{iNEWtracts}=newTractNames{iNEWtracts-20};
+end
+
 % posterior arcuate and temporo-parietal connection segmentation
 [L_pArc, ~, L_pArc_Indexes,L_TPC_Indexes ,R_pArc, ~, R_pArc_Indexes,R_TPC_Indexes] = bsc_automated_roi_segment_script_neo(wbFG,fsDIR);
 
-% Segment the major white matter tracts in the Mori Atlas
-[fg_classified,~,classification,~] = wma_majortracts(dti_file, wbFG);
+classification(L_pArc_Indexes)=23;
+classification(R_pArc_Indexes)=24;
+classification(L_TPC_Indexes)=25;
+classification(R_TPC_Indexes)=26;
 
 % Segment the Vertical Occipital Fasiculus (VOF)
 [~, ~, L_VOF_Indexes, R_VOF_Indexes] =  bsc_segmentVOF(wbFG, fsDIR, FiberDir, dtiFile, fg_classified(19), fg_classified(20), L_pArc, R_pArc, L_pArc_Indexes, R_pArc_Indexes);
 
+classification(L_VOF_Indexes)=21;
+classification(R_VOF_Indexes)=22;
+
 % Middle Longitudinal Fasiculus segmentation
 [~, RightMdLFindexes, ...
  ~, LeftMdLFindexes] = bsc_segmentMdLF_neo(wbFG, fsDIR);
+ 
+classification(LeftMdLFindexes)=27;
+classification(RightMdLFindexes)=28;
 
 disp('Tracts segmentation complete');
 
-% Colelct all tracts in  signle strucutre
-% Mori tracts from the JHU Atrlas (Zhang 2008)
-tracts_indexes.L_ThalamicRadiation=find(  classification.index==1);
-tracts_indexes.R_ThalamicRadiation=find(  classification.index==2);
-tracts_indexes.L_CorticoSpinalTract=find( classification.index==3);
-tracts_indexes.R_CorticoSpinalTract=find( classification.index==4);
-tracts_indexes.L_CingulateCingulum=find(  classification.index==5);
-tracts_indexes.R_CingulateCingulum=find(  classification.index==6);
-tracts_indexes.L_CingulumHippocampus=find(classification.index==7);
-tracts_indexes.R_CingulumHippocampus=find(classification.index==8);
-tracts_indexes.CallosumForceMajor=find(   classification.index==9);
-tracts_indexes.CallosumForceMinor=find(   classification.index==10);
-tracts_indexes.L_IFOF=find(classification.index==11);
-tracts_indexes.R_IFOF=find(classification.index==12);
-tracts_indexes.L_ILF=find( classification.index==13);
-tracts_indexes.R_ILF=find( classification.index==14);
-tracts_indexes.L_SLF=find( classification.index==15);
-tracts_indexes.R_SLF=find( classification.index==16);
-tracts_indexes.L_Uncinate=find(classification.index==17);
-tracts_indexes.R_Uncinate=find(classification.index==18);
-tracts_indexes.L_Arcuate=find( classification.index==19);
-tracts_indexes.R_Arcuate=find( classification.index==20);
-
-% Vertical associative tracts
-tracts_indexes.L_VOF=L_VOF_Indexes;
-tracts_indexes.R_VOF=R_VOF_Indexes;
-tracts_indexes.L_pArc=L_pArc_Indexes;
-tracts_indexes.R_pArc=R_pArc_Indexes;
-tracts_indexes.L_TPC=L_TPC_Indexes;
-tracts_indexes.R_TPC=R_TPC_Indexes;
-tracts_indexes.R_MdLF=RightMdLFindexes;
-tracts_indexes.L_MdLF=LeftMdLFindexes;
-
 if ~nosave
-    %runs outlier removal with the given parameters
-    FiberIndexes= removeOutliersBSC(FiberIndexes,wbFG, 4, 4);
-    
-    %saves down indexes after
-    save (strcat(fullfile(FiberDir),saveHeader,'_FiberIndexes.mat'),'FiberIndexes','-v7.3');
-    
-    %creates a classification structure that is compatable with AFQ
-    classification = bsc_convertBSCtoAFQ (FiberIndexes, wbFG);
     
     save (strcat(fullfile(FiberDir),saveHeader,'classification.mat'),'classification','-v7.3');
+    fprintf(' \n classification saved \n')
 end
-
-fprintf(' \n Fiber Indexes saved \n');
 
 return
