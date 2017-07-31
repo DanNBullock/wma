31f0b9a4fc415f16b2bff6f7f5c53bdb52c0031f
DanNBullock <dnbulloc@umail.iu.edu>
2017-07-31 11:24:25 -0400

    Update wma_multiTractAnalysis.m
    Impliment Parse

--- utilities/wma_multiTractAnalysis.m
+++ utilities/wma_multiTractAnalysis.m
@@ -1,7 +1,7 @@
 function [tractStats] = wma_multiTractAnalysis(classification,feORwbFG,dt6)
 %[summaryStructure] = wma_multiTractAnalysis(tractNameList,fiberIndexList,feORwbFG,FiberDir,saveHeader,dt6,fsDIR)
 %
-% OVERVIEW: this function runs a bevy of analysis functions including 
+% OVERVIEW: this function runs a bevy of analysis functions including
 % bsc_multiVirtualLesion, bsc_multiTractVolumeFE, bsc_multiTractLengthFG,% (C) Daniel Bullock, 2017, Indiana University
 % and bsc_multiTractProfilesFE.  It combines the output from several of
 % these into a single summary structure and saves it down.
@@ -32,35 +32,21 @@ function [tractStats] = wma_multiTractAnalysis(classification,feORwbFG,dt6)
 % (C) Daniel Bullock, 2017, Indiana University
 %% preliminaries
 
-% loads file if a string was passed
-if ischar(feORwbFG)
-    load(feORwbFG);
-    %haven't been able to test this (path to wbFG passed in) yet.
-    %if it is a fe structure, get the wbFG out of it
-    if exist('fe','var')
-        WBFG = feGet(fe, 'fibers acpc');
-    elseif isfield(feORwbFG,'fg')
-        WBFG = feGet(fe, 'fibers acpc');
-        fe=feORwbFG;
-    else
-        fe=[];
-    end
-elseif isfield(feORwbFG,'fg')
-    WBFG = feGet(feORwbFG, 'fibers acpc');
-    fe=feORwbFG;
-    clear feORwbFG
-end
+% parses fe/wbfg input
+[WBFG, fe] = bsc_LoadAndParseFiberStructure(feORwbFG);
+
 
 % loads the dt6 file if a path was passed
 if ischar(dt6)
     dt6 = dtiLoadDt6(dt6);
 end
 
-
-
 %% iterative tract analysis
+
+
 for Itracts=1:length(classification.names)
     indices=find(classification.index==Itracts);
-[tractStats{Itracts}] = wma_singleTractAnalysis(indices,fe,WBFG,dt6);
+    [tractStats{Itracts}] = wma_singleTractAnalysis(indices,fe,WBFG,dt6);
+    
 end
 end
