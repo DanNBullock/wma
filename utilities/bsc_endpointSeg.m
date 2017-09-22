78a7440ecbde8537c5eefe8359c095bdfe4d8854
DanNBullock <dnbulloc@umail.iu.edu>
2017-09-22 14:50:00 -0400

    Add files via upload
    New tracts added

---
+++ utilities/bsc_endpointSeg.m
@@ -0,0 +1,8 @@
+function [fg, keep] =bsc_endpointSeg(fg, roi)
+%  [fg, keep] =bsc_endpointSeg(fg, roi)
+%  Replacement for dtiIntersectROI's endpoints function WHICH DOESN'T WORK
+
+
+            
+ 
+end
