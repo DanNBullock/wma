5a3c44e470fcfeed7381c9ef56e822560477dccf
DanNBullock <dnbulloc@umail.iu.edu>
2018-04-07 15:30:17 -0400

    Update bsc_LoadAndParseFiberStructure.m

--- utilities/bsc_LoadAndParseFiberStructure.m
+++ utilities/bsc_LoadAndParseFiberStructure.m
@@ -8,7 +8,7 @@ function [wbFG, fe] = bsc_LoadAndParseFiberStructure(feORwbfg)
 % which an img coordspace wbFG is passed.
 %
 % INPUTS
-% 
+%
 % -feORwbfg: either a string or an object, to either a wbFG or an FE
 %  structure
 %
@@ -23,30 +23,38 @@ function [wbFG, fe] = bsc_LoadAndParseFiberStructure(feORwbfg)
 % checks if passed variable is a path or an object
 if ischar(feORwbfg)
     % loads if a path
-    load(feORwbfg)
-    % function is agnostic about naming conventions, but utilizes
-    % regularity in structure fields in order to identify variables of
-    % interest.
-    varList=whos;
-    % for each variable in the workspace, check and see if it is either a
-    % life structure (if 'life' field is present) or an fg (if 'fibers'
-    % field is present.
-    for iVars=1:length(varList)
-        curVar=eval(varList(iVars).name);
-        if isfield(curVar, 'life')
-            fprintf('\n fe structure loaded \n')
-            % obtains fibers in acpc space
-            wbFG = feGet(curVar, 'fibers acpc');
-            fprintf('\n streamlines extracted to wbfg in acpc coordspace \n')
-            fe=curVar;
-        elseif isfield(curVar,'fibers')
-            fprintf('\n fg structure loaded.  \n fg structure is in %s coordpace \n', curvar.coordspace)
-            wbFG = curVar;
-            fe=[];
+    %if a .tck path is detected, use relevant function
+    if strcmp(feORwbfg(end-3:end),'.tck')
+        wbFG =  dtiImportFibersMrtrix(feORwbfg, .5);
+        fe=[];
+    else
+    %otherwise, use standard mba/vistasoft functions
+        load(feORwbfg)
+        % function is agnostic about naming conventions, but utilizes
+        % regularity in structure fields in order to identify variables of
+        % interest.
+        varList=whos;
+        % for each variable in the workspace, check and see if it is either a
+        % life structure (if 'life' field is present) or an fg (if 'fibers'
+        % field is present.
+        for iVars=1:length(varList)
+            curVar=eval(varList(iVars).name);
+            if isfield(curVar, 'life')
+                fprintf('\n fe structure loaded \n')
+                % obtains fibers in acpc space
+                wbFG = feGet(curVar, 'fibers acpc');
+                fprintf('\n streamlines extracted to wbfg in acpc coordspace \n')
+                fe=curVar;
+            elseif isfield(curVar,'fibers')
+                fprintf('\n fg structure loaded.  \n fg structure is in %s coordpace \n', curvar.coordspace)
+                wbFG = curVar;
+                fe=[];
+            end
         end
     end
     %if an object is passed, proceeds accordingly
 elseif isstruct(feORwbfg)
+    %FUNCTION HAS NO WAY TO HANDLE .TCK OBJECTS CURRENTLY.  FIGURE THIS OUT.
     if isfield(feORwbfg, 'life')
         fprintf('\n fe structure loaded')
         % obtains fibers in acpc space
@@ -61,4 +69,6 @@ else
     % errors if neither a structure nor a string is passed.  Dont know how
     % this would happen in typical use.
     error('\n input is neither a structure nor a string.\n')
+end
+
 end
